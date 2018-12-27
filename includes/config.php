3d4a98c64ec379b942debd92bb48814158f9586b
= <s.lakhani@hotmail.ca>
2018-12-27 13:17:05 -0500

    Initialized the database, and allowed the user name to be saved if there is an incorrect signup attempt.

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+    ob_start();
+    
+    $timezone = date_default_timezone_set("America/Toronto");
+    $con = mysqli_connect("localhost", "root", "", "musicPlayer");//server name, username, password, and database
+    if(mysqli_connect_errno()){
+        echo "Failed to connect with mysql: " . mysqli_connect_errno(); //period is to append strings
+    }
+?>
--- register.php
+++ register.php
@@ -2,11 +2,18 @@
 <!DOCTYPE html>
 
 <?php
+    include("includes/config.php");
     include("includes/classes/Account.php");
     include("includes/classes/Constants.php");
     $account = new Account();
     include("includes/handlers/registerHandler.php");
     include("includes/handlers/loginHandler.php");
+
+    function getInputValue($fieldName){
+        if(isset($_POST[$fieldName])){
+            echo $_POST[$fieldName];
+        }
+    }
 
 ?>
 <html>
@@ -20,7 +27,7 @@
             <form id= "loginForm" action="register.php" method="post">
                 <h2>Login to your account</h2>
                 <label for="loginUsername">Username</label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="Username" required><br>
+                <input id="loginUsername" name="loginUsername" type="text" placeholder="Username" value="<?php getInputValue('loginUsername')?>" required><br>
                 <label for="loginPassword">Password</label>
                 <input id="loginPassword" name="loginPassword" type="password" placeholder="Password" required><br>
                 <button type="submit" name="loginButton">Log in</button>
@@ -29,7 +36,7 @@
              <form id= "registerForm" action="register.php" method="post">
                 <h2>Create your account</h2>
                 <label for="registerUsername">Username</label>
-                <input id="registerUsername" name="registerUsername" type="text" placeholder="Username" required><br>
+                <input id="registerUsername" name="registerUsername" type="text" placeholder="Username" value="<?php getInputValue('registerUsername')?>" required><br>
                 <label for="registerPassword">Choose Password</label>
                 <input id="registerPassword" name="registerPassword" type="password" placeholder="Password" required><br>
                 <label for="confirmPassword">Confirm Password</label>
