84df808fc20075f08baa8cf0a23a69c3d9715521
= <s.lakhani@hotmail.ca>
2018-12-30 17:04:55 -0500

    Added functionality to allow the app to redirect to the main page once a login is successful

--- includes/handlers/loginHandler.php
+++ includes/handlers/loginHandler.php
@@ -6,7 +6,11 @@
        //Account login function
        $result = $account->login($username, $password);
        if($result){
+           $_SESSION['userLoggedIn'] = $username;
            header("Location: index.php");
+       }
+       else{
+            unset($_SESSION['userLoggedIn']);//No one is logged in if someone else is trying to login. TODO:Consider changing the behavior of this
        }
     }
     ?>
