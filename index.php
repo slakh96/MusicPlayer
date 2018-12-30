dbf780657dd8d3812c88290a083072dc47d4f964
= <s.lakhani@hotmail.ca>
2018-12-30 17:07:44 -0500

    Added a php check to see if the user is logged in; if not, then send them directly to register/login page

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+<?php
+    //session_destroy()//To logout manually
+    include('includes/config.php');
+    if (isset($_SESSION['userLoggedIn'])){
+        $userLoggedIn = $_SESSION['userLoggedIn'];
+    }
+    else{
+        header("Location: register.php");//send them to login/register if they are not logged in
+    }
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>
-            Welcome to Slotify!
-        </title>
+        <title>Welcome to Slotify!</title>
     </head>
     <body>
         Hello World!
