da1c01869737fc378a5a504d4a4f8b18acb3fd72
= <s.lakhani@hotmail.ca>
2018-12-30 17:03:27 -0500

    Added a query to enable login sessions to work in the application

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
     ob_start();
-    
+    session_start(); //Enables sessions to work
     $timezone = date_default_timezone_set("America/Toronto");
     $con = mysqli_connect("localhost", "root", "", "musicPlayer");//server name, username, password, and database
     if(mysqli_connect_errno()){
