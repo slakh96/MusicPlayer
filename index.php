d56c8aa4bb0ff0abe13489f4ea9b2351e43ca065
= <s.lakhani@hotmail.ca>
2019-01-02 11:14:15 -0500

    Created now playing bar with rewind, play, forward icons etc. Created a progress bar that will be used to show what portion of a song is played.

--- index.php
+++ index.php
@@ -26,7 +26,28 @@
                 <div class="nowPlayingCenter">
                     <div class="content playerControls">
                         <div class="buttons">
-                            <button>Hellow</button>
+                            <button class="controlButton shuffle" title="Shuffle" alt="Shuffle"><img src="assets/images/icons/icons8_Shuffle_25px.png"></button>
+                            
+                            <button class="controlButton rewind" title="rewind" alt="rewind"><img src="assets/images/icons/icons8_Rewind_25px.png"></button>
+                            
+                            <button class="controlButton play" title="Play" alt="Play"><img src="assets/images/icons/icons8_Circled_Play_25px_1.png"></button>
+                            
+                            <button class="controlButton pause" title="Pause" alt="Pause" style="display:none"><img src="assets/images/icons/icons8_Pause_Button_25px_1.png"></button>
+                            
+                            <button class="controlButton forward" title="Forward" alt="Forward"><img src="assets/images/icons/icons8_Fast_Forward_25px.png"></button>
+                            
+                            <button class="controlButton loop" title="Replay" alt="Replay"><img src="assets/images/icons/icons8_Repeat_25px.png"></button>
+                            
+                        </div>
+                        <div class="playbackBar">
+                            <span class = "progressTime current">0.00</span>
+                            <div class="progressBar">
+                                <div class="progressBarBackground">
+                                    <div class="progress"></div>
+                                </div>
+                                
+                            </div>
+                            <span class = "progressTime remaining">0.00</span>
                         </div>
                     </div>
                 </div>
